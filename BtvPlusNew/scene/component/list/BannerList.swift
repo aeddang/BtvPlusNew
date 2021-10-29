8ae578021e2018be85435b7abcf78a1a27959472
aeddang <Aeddang@sk.com>
2021-10-29 13:13:52 +0900

    4.4.7

--- BtvPlusNew/scene/component/list/BannerList.swift
+++ BtvPlusNew/scene/component/list/BannerList.swift
@@ -129,7 +129,6 @@ struct BannerItem: PageView {
                 ImageView(
                     url: self.data.image,
                     contentMode:  SystemEnvironment.isTablet ? .fit : .fill,
-                    isFull: true,
                     noImg: self.data.type.noImage)
                     .frame(width: size.width, height: size.height)
                
